f6f7a84d7b87bc9aefc3103373e363a1837e5cf6
VG-Correa <v.victorgabriel2014@gmail.com>
2023-09-22 21:14:05 -0300

    Adicionado opções de configuração de raças

---
+++ config/settings.py
@@ -0,0 +1,33 @@
+CONFIG_RACAS = {
+    
+    "Humano": {
+        "Modificadores_atributos":
+            {   
+                'forca': 0,
+                'destreza': 0,
+                'constituicao': 0,
+                'inteligencia': 0,
+                'sabedoria': 0,
+                'carisma': 0,
+            },
+        "Modificadores_proficiencia":
+            {
+                "Armas": 
+                    {
+                        "arma_teste": 0,
+                    },
+                "Vestimentas":
+                    {
+                        "vestimenta_teste": 0,
+                    },
+                "Itens":
+                    {
+                        "item_teste": 0,
+                    }
+            }
+    }
+    
+    
+    
+    
+}
